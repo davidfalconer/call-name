e65ef530199da452694d1efc12a3637fddfed6e7
David Falconer <davidmichaelfalconer@gmail.com>
2016-01-09 18:19:04 +1100

    initial check in

---
+++ app.js
@@ -0,0 +1,20 @@
+// return David Falconer
+
+let name="David Falconer";
+let lastName="Falconer";
+
+function callName(){
+
+	return name;
+}
+
+function callLastName(){
+
+	return lastName;
+}
+
+module.exports = {
+	callName,
+	callLastName
+}
+
